d86b8328261671e95236ad200ce78d3cffb13c60
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-12 21:38:15 -0700

    generate questions using open trivia DB

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+import requests
 
+API_URL = "https://opentdb.com/api.php"
+API_PARAMETERS = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+# ------------ GET QUESTION DATA FROM OPEN TRIVIA DB ------------ #
+response = requests.get(url=API_URL, params=API_PARAMETERS)
+response.raise_for_status()
+question_data = response.json()["results"]
+
+
+# ------------------ CREATE QUESTION BANK ----------------------- #
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -9,11 +22,11 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+# ------------------ CREATE AND RUN QUIZ ------------------------ #
 quiz = QuizBrain(question_bank)
-
 while quiz.still_has_questions():
     quiz.next_question()
 
+# ------------------- END OF PROGRAM ---------------------------- #
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
